b687fba94841e78b7bdd32b94f0f325701e80c1b
LLaCorte <lorenzolacorte99@gmail.com>
2022-11-25 17:12:31 +0100

    first

--- AnonymizationCAHD.py
+++ AnonymizationCAHD.py
@@ -15,7 +15,7 @@ class AnonymizationCAHD:
     all_sensitive_transactions = None
     sensitive_items_for_transaction = None # list sd for transaction
 
-    dict_group = None
+    dict_group = None 
     anonymized_dataframe = None
     group_list = None
     group_sd = None
@@ -37,13 +37,24 @@ class AnonymizationCAHD:
 
         for s in self.sensitive_items:
             self.hists.append(int((self.bandwith_dataframe[s].sum()))) # one histogram
+        
+        
 
     # extract the same p-1 sensitive items with closest ID to t and unite it to t 
-    def constructG(self, t, CL):
-        G = [t]
-        for i in range(self.privacy_level-1):
-            G += min(CL, key=lambda x:abs(x-t))
+    def constructG(self, t, idx, CL):
+        G = {}
+        G[idx] = t
 
+        diff = {}
+        for i, x in enumerate(CL):
+            diff[i] = abs(x-t)
+        
+        tmp = sorted(diff.items(), key=lambda x:x[1])
+        for i in range(self.privacy_level-1):
+            tup = tmp[i]
+            G[tup[0]] = CL[tup[0]]
+        return G
+    
     def CAHD_algorithm(self):
         # number of remaining rows
         remaining = len(self.bandwith_dataframe.index)
@@ -53,6 +64,22 @@ class AnonymizationCAHD:
                 starting_idx = max(0, i-self.alfa*self.privacy_level)
                 ending_idx = min(len(self.sensitive_items), i+self.alfa*self.privacy_level)
                 CL = self.sensitive_items[starting_idx:i] + self.sensitive_items[i+1:ending_idx] 
-                G = self.constructG(self.hists[i], CL)
+                G = self.constructG(self.sensitive_items[i], i, CL)
+                self.dict_group = G
+                self.group_list = list(G.keys())
+                self.group_sd = list(G.values())
 
+                hist_copy = self.hists.copy()
+                for k, v in G.items():
+                    self.hists[k] = v
+
+                rollback = False
+                for s in self.hists:
+                    if(s * self.privacy_level <= remaining):
+                        rollback = True
+                
+                if(rollback): 
+                    self.hists = hist_copy
+                else:
+                    remaining -= len(G.keys())
             i+=1
